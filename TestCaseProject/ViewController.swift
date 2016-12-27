8a5606f299a3da55598634a250d95e2a4998cc4a
NPRABHAK-AMGEN <amgen@ltamb00205.local>
2016-12-27 15:50:13 +0530

    Initial Commit

---
+++ TestCaseProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestCaseProject
+//
+//  Created by amgen on 24/11/16.
+//  Copyright © 2016 amgen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
